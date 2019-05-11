b92eb947970bdbb425a44cea7d296c4540806dc6
yc2768 <2768647687@qq.com>
2019-05-11 16:39:00 +0800

    创建vue,config.js
    创建vue,config.js 配置引入main.scss ,在main.scss引入px2rem.scss。在App.Vue中引入common.css。修改router.js 使其指向Login页面

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Login</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    css: {
+        loaderOptions: {
+            // 给 sass-loader 传递选项
+            sass: {
+                // @/ 是 src/ 的别名
+                // 所以这里假设你有 `src/variables.scss` 这个文件
+                // data: `@import "./src/lib/hotcss/px2rem.scss";`
+                data: `@import "./src/assets/style/main.scss";`
+            }
+        }
+    },
+    devServer: {
+        proxy: {
+            "/api": {
+                target: "http://yapi.demo.qunar.com",
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    "^/api": "/mock/24076/api" // rewrite path
+                }
+            }
+        }
+    }
+};
